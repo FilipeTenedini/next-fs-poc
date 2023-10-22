d60f62dbe364befff2619df39cf9bffb8d1c62f3
FilipeTenedini <tenedinifilipe@gmail.com>
2023-10-22 10:33:16 -0300

    refact: update import at rootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Header from '@/components/ui/Header'
+import Header from '@/components/layout/header/Header'
 import { AuthProvider } from '@/providers/auth'
 
 const inter = Inter({ subsets: ['latin'] })

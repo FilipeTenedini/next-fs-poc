93df87c23a38fbf3e2bbbd86e8f499d03019da99
FilipeTenedini <tenedinifilipe@gmail.com>
2023-10-21 21:24:11 -0300

    refact: change components name to pascalcase

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,9 +1,16 @@
+"use client";
+
 import { HomeIcon, ListOrderedIcon, LogInIcon, MenuIcon, PercentIcon, ShoppingCartIcon } from "lucide-react";
-import { Button } from "./button";
-import { Card } from "./card";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import { Button } from "./Button";
+import { Card } from "./Card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./Sheet";
+import { signIn } from "next-auth/react";
 
 const Header = () => {
+  const handleLoginClick = async () => {
+    await signIn();
+  }
+
   return (
     <Card className="flex justify-between p-[1.875rem]">
       <Sheet>
@@ -19,7 +26,7 @@ const Header = () => {
           </SheetHeader>
 
           <div className="mt-2 flex flex-col gap-2">
-            <Button variant="outline" className="w-full justify-start gap-2">
+            <Button onClick={handleLoginClick} variant="outline" className="w-full justify-start gap-2">
               <LogInIcon size={16}/>
               Fazer Login
             </Button>

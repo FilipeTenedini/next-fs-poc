315939a18f006f149d3dcf2a85f429c40008c148
FilipeTenedini <tenedinifilipe@gmail.com>
2023-10-22 10:46:59 -0300

    feat: add first discount banner to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        height={0}
+        width={0}
+        alt="Até 55% de desconto este mês"
+        sizes="100vw"
+        className="h-auto w-full"
+      />
+    </div>
+  )
+}

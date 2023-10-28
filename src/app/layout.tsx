697c640a690d30aef568a4a6ba1fe88e9fbe8e43
FilipeTenedini <tenedinifilipe@gmail.com>
2023-10-28 10:36:28 -0300

    feat: ensure footer is always on the botton with style in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,13 +20,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
-          <Header />
-          {children}
-          <Footer />
-        </AuthProvider>
+        <div className="h-full flex flex-col">
+          <AuthProvider>
+            <Header />
+            <div className="flex-1">
+              {children}
+            </div>
+            <Footer />
+          </AuthProvider>
+        </div>
       </body>
-
     </html>
   )
 }

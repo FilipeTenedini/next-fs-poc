284ac1e1ade1c86b098e1b67a3115cc48ebd3cac
FilipeTenedini <tenedinifilipe@gmail.com>
2023-10-28 11:20:30 -0300

    feat: create catalog page

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,6 +1,25 @@
-const CatalogPage = () => {
+import { Badge } from "@/components/ui/Badge";
+import { prismaClient } from "@/lib/prisma";
+import { ShapesIcon } from "lucide-react";
+import CatalogItem from "./components/CatalogItem";
+
+const CatalogPage = async () => {
+  const categories = await prismaClient.category.findMany({});
+
   return (
-    <h1>catalog  </h1>
+    <div className="flex flex-col gap-8 p-5">
+      <Badge className="w-fit gap-1 border border-primary px-3 py-1 text-base uppercase" variant="outline">
+        <ShapesIcon size={16}/>
+        Catálogo
+      </Badge>
+
+      <div className="grid grid-cols-2 gap-7">
+        {categories.map((category) => (
+          <CatalogItem key={category.id} category={category} />
+        ))}
+      </div>
+
+    </div>
   );
 }
 

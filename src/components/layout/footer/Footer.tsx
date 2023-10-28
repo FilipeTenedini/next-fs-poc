11b66b42d0152b853300f9d02451e1315d9071b8
FilipeTenedini <tenedinifilipe@gmail.com>
2023-10-28 10:21:11 -0300

    feat(footer): create component and add him

---
+++ src/components/layout/footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-accent px-8 py-4 text-xs opacity-75">
+      © 2023 With <span className="text-primary text-xl ">♥</span> by <span className="font-semibold">Filipe Tenedini</span>.
+    </div>
+  );
+}
+
+export default Footer;
